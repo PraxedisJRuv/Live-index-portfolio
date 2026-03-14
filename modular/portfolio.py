e0a368c4a704463adff951c3b143ae97504b1c73
PraxedisJRuv <praxjruv@gmail.com>
2026-03-13 18:58:59 -0600

    Added portfolio value calculations

---
+++ modular/portfolio.py
@@ -0,0 +1,19 @@
+from benchmarks import amount_of_periods
+def portfolio_value(benchmark, df, period, tickers):
+    periods=amount_of_periods(period)
+    portfolio=[0]*periods
+    for j in range(len(tickers)):
+        valores=df[f"{tickers[j]} Close"].resample(period).mean()
+        for i in range(periods):
+            portfolio[i]=portfolio[i]+benchmark[i][j]*valores[i]
+    return portfolio
+
+def portfolio_vlaue_by_asset(benchmark, df, period, tickers):
+    periods=amount_of_periods(period)
+    portfolio=[]
+    for j in range(len(tickers)):
+        valores=df[f"{tickers[j]} Close"].resample(period).mean()
+        portfolio.append([])
+        for i in range(periods):
+            portfolio[j].append(benchmark[i][j]*valores[i])
+    return portfolio
