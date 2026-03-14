802e812298ebb80399a521ac16db8d4a5750f1cd
PraxedisJRuv <praxjruv@gmail.com>
2026-03-14 09:00:36 -0600

    Added Dashboard template to start working on, the index wont be real for now since that info is harder to get, now the dashboard is currently just for testing

--- modular/main_module.py
+++ modular/main_module.py
@@ -2,12 +2,15 @@ import pandas as pd
 from datetime import datetime, timedelta
 from extraction import full_dataframe_extraction
 import benchmarks as bm
+import portfolio as port
 tickers = ["AAPL.US","MSFT.US"]
 end=datetime.today()
 start=end-timedelta(days=2*365)
 df=full_dataframe_extraction(tickers, start, end)
 period=pd.Timedelta("2W")
-ew=bm.calc_EW(tickers,period)
-print(ew)
+#ew=bm.calc_EW(tickers,period)
+#print(ew)
 vola=bm.calc_vola(df,tickers,period)
 print(vola)
+portafolio=port.portfolio_value(vola,df,period,tickers)
+print(portafolio)
