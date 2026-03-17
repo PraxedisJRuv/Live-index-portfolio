a9c18c5eedd252b11ef07d1c299a5c326464355b
PraxedisJRuv <praxjruv@gmail.com>
2026-03-16 18:30:30 -0600

    sucessfull tests of pybind11

--- modular/optimization/comunication/test2/setup.py
+++ modular/optimization/comunication/test2/setup.py
@@ -1,11 +1,31 @@
-import numpy as np
-import miprocesador
+from setuptools import setup, Extension
+import pybind11
 
-A = np.array([
-    [1.0,2.0,3.0],
-    [4.0,5.0,6.0]
-])
+ext_modules=[
+    Extension(
+        "miprocesador",
+        ["procesador.cpp"],
+        include_dirs=[pybind11.get_include()],
+        language="c++"
+    )
+]
 
-B = miprocesador.procesar(A)
+setup(
+    name="miprocesador",
+    ext_modules=ext_modules,
+)
+"""
+La clase Extension define un módulo de extensión con
+nombre "miprocesador", junto las referencias para
+hallar el archivo de c++
+el get include es para que los encabezados se incluyan
+al momento de compilar el código de c++
 
-print(B)
+Veamos si funciona
+python setup.py build
+Esto para la configuración
+Se generó correctamente
+
+python setup.py install
+Esto para compilar
+"""
