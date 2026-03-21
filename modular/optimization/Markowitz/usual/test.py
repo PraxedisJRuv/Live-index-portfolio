f3b398ffe89727b0918b921d99ec8c7bfde1b4a7
PraxedisJRuv <praxjruv@gmail.com>
2026-03-21 14:39:34 -0600

    minor corrections regarding Makrowitz cfuncitons

--- modular/optimization/Markowitz/usual/test.py
+++ modular/optimization/Markowitz/usual/test.py
@@ -9,7 +9,7 @@ Sigma = Sigma.astype(np.float64)
 
 wb = np.array([0.1,0.03,0.7,0.07,0.1], dtype=np.float64)
 print(sum(wb))
-# señal de alpha (ejemplo)
+
 alpha = np.random.randn(n).astype(np.float64)
 
 lambda_ = 0.1
